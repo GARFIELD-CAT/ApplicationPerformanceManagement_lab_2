bca90f56ad902717836e1a645bff44fa3ea20413
Yagunov Denis <spb.jagunov.denis@gmail.com>
2026-02-04 19:33:33 +0300

    init project

---
+++ src/test/java/ru/dayagunov/lab_2/Lab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dayagunov.lab_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
